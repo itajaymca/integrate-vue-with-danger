325e3b20764f1dacad24120213802e45bdf6ed22
itajaymca <itajaymca@gmail.com>
2023-05-23 19:20:10 +0530

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <button class="btn" v-show="loggedIn">Logout</button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  export default {
+    name: 'app-header',
+    data() {
+      return {
+        loggedIn: false
+      }
+    }
+  }
+  </script>
