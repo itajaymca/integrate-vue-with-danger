9458399d14216a14cecd4dcd64c397404ef959e7
Ajay Sharma <Ajay.Sharma@belden.com>
2023-05-24 18:39:55 +0530

    updated danger file

--- dangerfile.js
+++ dangerfile.js
@@ -2,13 +2,17 @@
 import { message, warn, fail, markdown } from "danger"
 
 // Add a message to the table
-message("You have added 2 more modules to the app")
+message("This is a normal message")
 
 //  Adds a warning to the table
-warn("You have not included a CHANGELOG entry.");
+warn("This is a warning.");
 
 // Declares a blocking 
-fail(`ESLint has failed with ${fails} fails.`);
+fail(`This is a failure message.`);
 
 // Show markdown under the table:
 markdown("## New module Danger");
+
+const { additions =0, deletions = 0 } = danger.github.pr;
+
+message(`:tada: The PR added ${additions} and removed ${deletions} line.`);
